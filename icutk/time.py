818288625a33b643aefdd6032cdc6c5f379316b6
YEUNGCHIE <yeungchie@yeungchie.top>
2025-01-13 04:55:03 +0800

    支持配合with语句使用

--- icutk/time.py
+++ icutk/time.py
@@ -1,22 +1,43 @@
+from __future__ import annotations
 from time import perf_counter
+from typing import Any, Callable, Optional
 
 from .log import getLogger
 
+__all__ = [
+    "MeasureTime",
+    "measureTime",
+]
 
-def measureTime(func):
-    def wrapper(*args, **kwargs):
+
+class MeasureTime:
+    def __init__(self, func: Optional[Callable] = None) -> None:
+        self.logger = getLogger()
+        self.func = func
+
+    def __call__(self, *args, **kwargs) -> Any:
+        if self.func is None:
+            raise ValueError("No function provided")
         start_time = perf_counter()
         error = None
         try:
-            result = func(*args, **kwargs)
+            result = self.func(*args, **kwargs)
         except Exception as e:
             error = e
         end_time = perf_counter()
-        getLogger().info(
-            f"Function {func.__name__!r} took {end_time - start_time} seconds to run"
+        self.logger.info(
+            f"Procedure {self.func.__name__!r} took {end_time - start_time} seconds to run"
         )
         if error:
             raise error
         return result
 
-    return wrapper
+    def __enter__(self) -> MeasureTime:
+        self.start = perf_counter()
+        return self
+
+    def __exit__(self, *args, **kwargs) -> None:
+        self.logger.info(f"Program took {perf_counter() - self.start} seconds to run")
+
+
+measureTime = MeasureTime
